57d1926fd8cd842e96542e74192121db7b6b7f8a
Balthair94 <archerbaltazar@gmail.com>
2017-08-10 01:15:39 -0500

    View completed

--- app/src/main/java/baltamon/mx/tictactoe/MainActivity.java
+++ app/src/main/java/baltamon/mx/tictactoe/MainActivity.java
@@ -1,7 +1,11 @@
 package baltamon.mx.tictactoe;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupToolbar();
+    }
+
+    public void setupToolbar(){
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitleTextColor(Color.WHITE);
+        setSupportActionBar(toolbar);
+
+        if (getSupportActionBar() != null){
+            getSupportActionBar().setTitle("Tic Tac Toe");
+        }
+    }
+
+    public void onCellClicked(View view){
+        Log.i("Message", "Hola");
     }
 }
